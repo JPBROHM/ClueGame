262425c2228f33f000f3ab066710593d4aa5db59
Josh Brohm <Brohm@mymail.mines.edu>
2020-11-07 16:39:45 -0700

    added panels to then display panel

--- src/clueGame/GameControlPanel.java
+++ src/clueGame/GameControlPanel.java
@@ -60,7 +60,85 @@ public class GameControlPanel extends JPanel{
 
     public static void main(String[] args) {
     	
-    	
+    	GameControlPanel panel = new GameControlPanel();  // create the panel
+    	panel.setLayout(new GridLayout(2, 0));
+    	JFrame frame = new JFrame();  // create the frame
+    	frame.setContentPane(panel); // put the panel in the frame
+    	frame.setSize(750, 180);  // size the frame
+    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // allow it to close
+
+    	// test filling in the data
+    	Color color = new Color(0,0,0);
+    	color = Color.YELLOW;
+    	panel.setTurn(new ComputerPlayer( "Col. Mustard", color, 0, 0), 5);
+    	panel.setGuess( "I have no guess!");
+    	panel.setGuessResult( "So you have nothing?");
+
+
+
+        GameControlPanel top = new GameControlPanel();
+        top.setLayout(new GridLayout(1, 4));
+        
+        
+        
+        GameControlPanel topLeft = new GameControlPanel();
+        topLeft.setLayout(new GridLayout(2, 0));
+        
+
+        
+        
+        GameControlPanel topMiddleLeft = new GameControlPanel();
+        topMiddleLeft.setLayout(new GridLayout(0, 2));
+       
+        
+        
+        
+        
+        
+               
+        GameControlPanel bottom = new GameControlPanel();
+        bottom.setLayout(new GridLayout(0, 2));
+        
+        GameControlPanel bottomLeft = new GameControlPanel();
+        bottomLeft.setBorder(new TitledBorder (new EtchedBorder(), "Guess"));
+        bottom.setLayout(new GridLayout(1, 0));
+        
+        
+        GameControlPanel bottomRight = new GameControlPanel();
+        bottomRight.setBorder(new TitledBorder (new EtchedBorder(), "Guess Result"));
+        bottom.setLayout(new GridLayout(1, 0));
+        
+    	JLabel roll = new JLabel("               Roll:");
+        JLabel whosTurn = new JLabel("Who's turn?");
+        JButton accuseButton = new JButton("Make Accusation");
+        JButton nextButton = new JButton("NEXT!");
+
+        bottomRight.add(panel.getTheGuessResult());
+        bottomLeft.add(panel.getTheGuess());
+
+        bottom.add(bottomLeft);
+        bottom.add(bottomRight);
+
+        topLeft.add(whosTurn);
+        topLeft.add(panel.playerName);
+
+        topMiddleLeft.add(roll);
+        topMiddleLeft.add(panel.getRollNumber());
+        
+        top.add(topLeft);
+        top.add(topMiddleLeft);
+        top.add(accuseButton);
+        top.add(nextButton);
+        
+        panel.add(top);
+        panel.add(bottom);
+        
+        frame.setVisible(true);
+        
+
+
+
+
  }
 
 	
